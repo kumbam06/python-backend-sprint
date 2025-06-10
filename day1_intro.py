7781d4e7f5536bb6e659fb1a73c38ff50e813ca8
Anvilabs <developers@anvilabs.com>
2025-06-10 11:09:07 +0100

    Day 1: Input, variables, and basic types

---
+++ day1_intro.py
@@ -0,0 +1,20 @@
+#Ask for user details
+
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+
+# Print formatted output
+print(f"Hi {name}, You are {age} years old.")
+
+# Calculate future age
+
+future_age = age + 5
+print(f"In 5 years, you'll be {future_age} years old.")
+
+# Basic data types
+
+pi = 3.14159
+is_python_fun = True
+
+print(f"Pi value is {pi}")
+print(f"Is Python fun? {is_python_fun}")
